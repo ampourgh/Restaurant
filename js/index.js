7e5ea2a107b2b390c90c12f90f56e8c11299426b
Amir Pourghaed <ampourgh@gmail.com>
2018-02-10 15:51:51 -0600

    feat: separate list from main index.js file

--- js/index.js
+++ js/index.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { PizzaClass } from './PizzaList';
 
-const pizzas = [{name: 'Pepperoni', img: 'blank'},
-                {name: 'Mushrooms', img: 'blank'},
-                {name: 'Onions', img: 'blank'},
-                {name: 'Sausage', img: 'blank'},
-                {name: 'Bacon', img: 'blank'},
-                {name: 'Extra cheese', img: 'blank'},
-                {name: 'Black olives', img: 'blank'},
-                {name: 'Green peppers', img: 'blank'},
-                {name: 'Pineapple', img: 'blank'},
-                {name: 'Spinach', img: 'blank'}];
+class PizzaDisplay extends React.Component {
+  render() {
 
-const pizzaList = pizzas.map((pizza, i) =>
-  <div>
-	 <div key={'pizza_' + i}>{pizza.name}</div>
-   <img src={pizza.img}
-        alt={pizza.name}/>
-  </div>
-
-);
+    return (
+      <div>
+        <h1>Pizza Toppings:</h1>
+        <h2><PizzaClass /></h2>
+      </div>
+    );
+  }
+}
 
 // ReactDOM.render goes here:
-ReactDOM.render(<ul>{pizzaList}</ul>,
-		document.getElementById('app'))
+ReactDOM.render(<PizzaDisplay />, document.getElementById('app'))
