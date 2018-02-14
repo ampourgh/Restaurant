82e9eb87e10a53832d95bbb9ddecece8a7a99adc
Amir Pourghaed <ampourgh@gmail.com>
2018-02-13 22:49:52 -0600

    feat: place list items in menu.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ContactsList from './ContactsList'
-
-let contacts = [{
-  id: 1,
-  name: 'Nolan',
-  phone: '5'
-}, {
-  id: 2,
-  name: 'Stephen',
-  phone: '1'
-}, {
-  id: 3,
-  name: 'Stan',
-  phone: '2'
-}]
+import Menu from './Menu'
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Contacts:</h1>
-        <ContactsList contacts={this.props.contacts}/>
+        <h1>Toppings:</h1>
+        <Menu />
       </div>
     )
   }
 }
 
-ReactDOM.render(<App contacts={contacts} />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
