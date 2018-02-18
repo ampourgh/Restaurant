a49ff1d3b0b0ca2ff2f3cd05a8f60a49cf0165cb
Amir Pourghaed <ampourgh@gmail.com>
2018-02-17 21:14:23 -0600

    feat: incorporate flexbox and image in JSX

--- src/item.js
+++ src/item.js
@@ -5,8 +5,8 @@ class Item extends React.Component {
     const menuItem = this.props.item;
     return (
 
-      <li>
-        {menuItem.name} ......... {menuItem.price}
+      <li className="flex-row">
+        <img className="image" src={menuItem.img}  />
       </li>
     )
   }
