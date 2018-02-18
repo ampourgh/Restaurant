ae34b0ebf938031ebf70b703a21199773f9238ae
Amir Pourghaed <ampourgh@gmail.com>
2018-02-17 21:12:43 -0600

    feat: render list from PizzaList.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Menu from './Menu'
+import { PizzaClass } from './PizzaList';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Toppings:</h1>
+        <p>Search for images of food with keywords</p>
         <Menu />
       </div>
     )
   }
 }
 
+class PizzaDisplay extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Toppings</h1>
+        <PizzaClass />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<PizzaDisplay />, document.getElementById('app2'))
 ReactDOM.render(<App />, document.getElementById('app'));
