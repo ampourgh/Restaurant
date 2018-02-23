3c424cdb4e8a7bf3dde2403d394437917d4057d6
Amir Pourghaed <ampourgh@gmail.com>
2018-02-23 15:57:38 -0600

    chore: various adjustments to css and jsx
    Adjustments include redesigning the footer aesthetic, modifying the JSX to include a dropdown enlarged image, etc.

---
+++ src/Item.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Item extends React.Component {
+  render() {
+    const menuItem = this.props.item;
+    return (
+
+        <div className="w3-dropdown-hover">
+          <li className="flex-row">
+            <img className="image" src={menuItem.img} />
+            <div className="w3-dropdown-content">
+              <img className="dropdown-image" src={menuItem.img} />
+            </div>
+          </li>
+
+        </div>
+
+    )
+  }
+}
+
+export default Item;
---
+++ src/Menu.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Item from './Item';
+
+class Menu extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: ''
+    };
+  }
+
+  updateSearch(event) {
+    this.setState({search: event.target.value.substr(0, 20)});
+  }
+
+  render() {
+    const menuItems = [
+                      {id: 1, name: 'Pepperoni', price: '.99', img: '../images/pictures/pizza1.jpg'},
+                      {id: 2, name: 'Mushrooms', price: '.49', img: '../images/pictures/pizza2.jpg'},
+                      {id: 3, name: 'Onions', price: '.49', img: '../images/pictures/pizza3.jpg'},
+                      {id: 4, name: 'Sausage', price: '.99', img: '../images/pictures/pizza4.jpg'},
+                      {id: 5, name: 'Bacon', price: '.99', img: '../images/pictures/pizza5.jpg'},
+                      {id: 6, name: 'Extra cheese', price: '.49', img: '../images/pictures/1.jpg'},
+                      {id: 7, name: 'Black olives', price: '.49', img: '../images/pictures/1.jpg'},
+                      {id: 8, name: 'Green peppers', price: '.49', img: '../images/pictures/1.jpg'},
+                      {id: 9, name: 'Pineapple', price: '.49', img: '../images/pictures/1.jpg'},
+                      {id: 10, name: 'Pineapple', price: '.49', img: '../images/pictures/1.jpg'},
+                      {id: 11, name: 'Pineapple', price: '.49', img: '../images/pictures/1.jpg'},
+                      {id: 12, name: 'Spinach', price: '.49', img: '../images/pictures/1.jpg'}
+                    ];
+
+    let filteredItems = menuItems.filter(
+      (item) => {
+        return item.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+      }
+    );
+
+    return (
+      <div>
+        <img className="search-icon" src="../images/search-icon.png"></img>
+        <input type="text" value={this.state.search} onChange={this.updateSearch.bind(this)}/>
+        <ul>
+          {filteredItems.map((item)=> {
+              return <Item item={item} key={item.id}/>
+          })}
+        </ul>
+      </div>
+    )
+
+  }
+}
+
+export default Menu;
