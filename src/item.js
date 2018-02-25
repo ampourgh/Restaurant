e628a64d0b9666355bee35a7ec84981fd2b72138
Amir Pourghaed <ampourgh@gmail.com>
2018-02-25 12:51:36 -0600

    feat: image modal using react bootstrap

--- src/item.js
+++ src/item.js
@@ -1,15 +1,44 @@
 import React from 'react';
 
-class Item extends React.Component {
+var Button = ReactBootstrap.Button;
+var Modal = ReactBootstrap.Modal;
+
+
+const Item = React.createClass({
+
+
+  getInitialState() {
+    return { showModal: false };
+  },
+
+  close() {
+    this.setState({ showModal: false });
+  },
+
+  open() {
+    this.setState({ showModal: true });
+  },
+
   render() {
     const menuItem = this.props.item;
     return (
+      <div>
+        <li className="flex-row">
+          <Button onClick={this.open}>
+            this
+          </Button>
+          <img className="image" src={menuItem.img} />
+        </li>
 
-      <li className="flex-row">
-        <img className="image" src={menuItem.img}  />
-      </li>
-    )
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Body>
+            <img className="image" src={menuItem.img} />
+          </Modal.Body>
+        </Modal>
+      </div>
+
+    );
   }
-}
+});
 
 export default Item;
